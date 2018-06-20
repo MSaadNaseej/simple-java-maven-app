485abc4ecaa109e65b29ca4386db62460fac389b
CAIRO\MSaad <Mahmoud.Saad@naseej.com>
2018-06-20 15:28:53 +0200

    new commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,6 +13,7 @@ public class App
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
       System.out.println("jenkinse");
+      System.out.println("hello");
     }
 
     private final String getMessage() {
